6bd4a847453d90ce395d9980b6e4f0b264c0b5e8
302A-05\cre <a@a>
2026-07-07 12:07:12 +0900

    workEX1 Q4拡張マガジンつけるところから

--- works/workEx1/Main.php
+++ works/workEx1/Main.php
@@ -3,9 +3,21 @@
 // Userクラスの読み込み
 require 'Gun.php';
 
-$mainWeapon = new Gun("AK47",30);
-$subWeapon = new Gun("Mosin-Nagant",5);
+$mainWeapon = new Gun("AK47", 30);
+$subWeapon = new Gun("Mosin-Nagant", 5);
 
 // 現在の状態を表示
+$mainWeapon->echoStatus();
+$subWeapon->echoStatus();
+
+$mainWeapon->setExtendedMagazine();
+$subWeapon->setExtendedMagazine();
+
+$mainWeapon->reload();
+$subWeapon->reload();
+
+$mainWeapon->fire();
+$subWeapon->fire();
+
 $mainWeapon->echoStatus();
 $subWeapon->echoStatus();
